bff9c0319a099e68ea8cdd1507d619a34f1d89bb
manojitroy <roy.monojit982810@gmail.com>
2024-08-25 18:11:46 +0530

    Add project to create using lombok

---
+++ src/main/java/User.java
@@ -0,0 +1,14 @@
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Builder
+public class User {
+    private String firstName;
+    private String lastName;
+    private String enail;
+}
